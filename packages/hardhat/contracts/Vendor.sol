c62c90272e4873739eb4e925b47f46ca1ab23573
TheWalker <adriahernandez.2001@gmail.com>
2022-11-09 11:43:03 +0100

    Started challenge 2

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -1,23 +1,46 @@
-pragma solidity 0.8.4;  //Do not change the solidity version as it negativly impacts submission grading
+pragma solidity 0.8.4; //Do not change the solidity version as it negativly impacts submission grading
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
 
+error Vendor__YouMustBeTheOwner();
+error Vendor__TransferFailed();
+
 contract Vendor is Ownable {
+    event BuyTokens(
+        address indexed buyer,
+        uint256 amountOfETH,
+        uint256 amountOfTokens
+    );
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    YourToken public yourToken;
 
-  YourToken public yourToken;
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    uint256 public constant tokensPerEth = 100;
 
-  // ToDo: create a payable buyTokens() function:
+    // ToDo: create a payable buyTokens() function:
+    function buyTokens() public payable {
+        uint256 amount = msg.value * tokensPerEth;
+        yourToken.transfer(msg.sender, amount);
+        emit BuyTokens(msg.sender, msg.value, amount);
+    }
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function withdraw() public {
+        if (msg.sender != owner()) {
+            revert Vendor__YouMustBeTheOwner();
+        }
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
+        if (!callSuccess) {
+            revert Vendor__TransferFailed();
+        }
+    }
 
-  // ToDo: create a sellTokens(uint256 _amount) function:
-
+    // ToDo: create a sellTokens(uint256 _amount) function:
 }
